f5d4482e11ce29efcc49d143613f1cd912683b6e
mslxl <i@mslxl.com>
2023-04-11 21:11:58 +0800

    chore: init project

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import {RouterView} from 'vue-router'
+</script>
+
+<template lang="pug">
+.container
+  router-view
+</template>
+
+<style scoped lang="scss">
+.container {
+  width: 100vw;
+  height: 100vh;
+}
+</style>
