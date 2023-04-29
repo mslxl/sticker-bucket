938be9bb38d244082ef22a5120bdfd92a5201ef5
mslxl <i@mslxl.com>
2023-04-30 00:01:29 +0800

    feat: support system tray

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,14 +1,25 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::{CustomMenuItem, Manager, SystemTray, SystemTrayEvent, SystemTrayMenu};
+
 mod db;
-mod meme;
 mod error;
 mod handler;
+mod meme;
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 
+fn build_system_tray() -> SystemTray {
+    let item_quit = CustomMenuItem::new(String::from("quit"), "Quit");
+
+    let tray_menu = SystemTrayMenu::new().add_item(item_quit);
+
+    SystemTray::new().with_menu(tray_menu)
+}
 fn main() {
+    let tray = build_system_tray();
+
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![
             handler::database::open_image_and_interfere,
@@ -24,6 +35,39 @@ fn main() {
             handler::database::get_image_real_path,
             handler::debug::is_debug
         ])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+        .system_tray(tray)
+        .on_system_tray_event(|app, event| match event {
+            SystemTrayEvent::MenuItemClick { id, .. } => {
+                // let item_handle = app.tray_handle().get_item(&id);
+                match id.as_str() {
+                    "quit" => app.exit(0),
+                    _ => {}
+                }
+            }
+            SystemTrayEvent::LeftClick { .. } => {
+                let handle = app.app_handle();
+                std::thread::spawn(move || {
+                    let result = tauri::WindowBuilder::new(
+                        &handle,
+                        "main",
+                        tauri::WindowUrl::App("index.html".into()),
+                    )
+                    .title("Meme Management")
+                    .build();
+                    match result {
+                        Ok(_)  | Err(tauri::Error::WindowLabelAlreadyExists(_)) => {},
+                        Err(e) => panic!("{:?}", e)
+                    }
+                });
+            }
+            _ => {}
+        })
+        .build(tauri::generate_context!())
+        .expect("error while running tauri application")
+        .run(|_app_handle, event| match event {
+            tauri::RunEvent::ExitRequested { api, .. } => {
+                api.prevent_exit();
+            }
+            _ => {}
+        });
 }
